24a88d1fe96d7d0cab70c7e378ebe267974b5f1c
Chris Castellano <c.castellano18@gmail.com>
2022-07-02 17:46:46 -0500

    setup

--- Project2/dist/app.js
+++ Project2/dist/app.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log('Hello World');
+let user1;
+user1 = {
+    name: 'Chris',
+    age: 41,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
+    },
+};
+user1.greet('Hi there - I am');
 //# sourceMappingURL=app.js.map
--- Project2/src/app.ts
+++ Project2/src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hello World');
+interface Person {
+  name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+let user1: Person;
+
+user1 = {
+  name: 'Chris',
+  age: 41,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  },
+};
+
+user1.greet('Hi there - I am');
